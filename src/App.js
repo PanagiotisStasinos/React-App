a2629351d09383b9f5013a0ccce5866c3947191f
PanagiotisStasinos <panagiotis.stasin@gmail.com>
2022-05-23 16:55:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import React, { Component }  from 'react';
+
+function App() {
+  return (
+    <div >
+      <Header />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
